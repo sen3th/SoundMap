b63697548ee2555cdff76410e3b6c5f932779376
Seneth <126297209+sen3th@users.noreply.github.com>
2026-03-17 14:50:17 -0400

    minor styling + added footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,11 @@
+<footer>
+    <p class="text-center text-gray-500 text-sm mt-4">
+
+        <a href="https://github.com/sen3th/SoundMap" target="_blank" rel="noopener noreferrer" class="text-pink-600 hover:underline">GitHub</a>
+
+         | 
+
+         <a href="https://seneth.me" target="_blank" rel="noopener noreferrer" class="text-pink-600 hover:underline">seneth.me</a>
+        
+    </p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Map from '$lib/Map.svelte';
+    import Footer from '$lib/Footer.svelte';
 </script>
 
-<h1 class="text-3xl font-bold m-1">SoundMap</h1>
-<p class="text-lg text-gray-600 m-1">Discover Sounds from Different Places</p>
+<h1 class="text-3xl text-pink-600 font-bold ml-5 mb-1.5 mt-3 cursor-default">SoundMap</h1>
+<p class="text-lg text-gray-600 ml-5">Discover Sounds from Different Places</p>
 
 <Map/>
+<Footer/>
