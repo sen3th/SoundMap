ed8126d7b6ed2c37f4bf18af0784e2471d2136de
Seneth <126297209+sen3th@users.noreply.github.com>
2026-03-17 15:17:47 -0400

    added country display when clicked

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,5 +1,14 @@
 <script>
-   import { onMount } from 'svelte';
+    import { onMount } from 'svelte';
+
+
+    // country finder
+
+    let selectedCountry = '';
+    let marker;
+
+
+    //map setup
 
    onMount(async () => {
       const leaflet = await import('leaflet');
@@ -13,7 +22,33 @@
          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(map);
 
-      return () => map.remove();
+
+      // country finder
+        map.on('click', async (e) => {
+            const latitude = e.latlng.lat;
+            const longitude = e.latlng.lng;
+
+            selectedCountry = 'Loading...';
+
+         try {
+            const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+
+            const res = await fetch(url);
+            const data = await res.json();
+
+            selectedCountry = data.countryName || 'Country not found';
+
+            if (marker) marker.remove();
+            marker = L.marker ([latitude, longitude]).addTo(map).bindPopup(selectedCountry).openPopup();
+            } catch (error) {
+            selectedCountry = 'Error fetching country';
+         }
+
+         return () => map.remove();
+        });
+
+
+        return () => map.remove();
    });
 </script>
 
