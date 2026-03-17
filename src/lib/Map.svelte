9fdccc34cfa932fed241499ede2e833b01f88978
Seneth <126297209+sen3th@users.noreply.github.com>
2026-03-17 14:22:52 -0400

    added leaflet map

---
+++ src/lib/Map.svelte
@@ -0,0 +1,20 @@
+<script>
+   import { onMount } from 'svelte';
+
+   onMount(async () => {
+      const leaflet = await import('leaflet');
+      const L = leaflet.default ?? leaflet;
+      await import('leaflet/dist/leaflet.css');
+
+      const map = L.map('map').setView([0,0], 2);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         maxZoom: 19,
+         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map);
+
+      return () => map.remove();
+   });
+</script>
+
+<div id="map" class="m-5" style="height: 600px;"></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Map from '$lib/Map.svelte';
+</script>
+
+<h1 class="text-3xl font-bold m-1">SoundMap</h1>
+<p class="text-lg text-gray-600 m-1">Discover Sounds from Different Places</p>
+
+<Map/>
