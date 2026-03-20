cb0962089b4e1da27e6e5579b0dc497a26b216d1
Seneth <126297209+sen3th@users.noreply.github.com>
2026-03-20 14:31:11 -0400

    css styling

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="fixed bottom-0 w-full">
+<footer >
     <p class="text-center text-gray-500 text-sm mt-4 mb-3">
 
         <a href="https://github.com/sen3th/SoundMap" target="_blank" rel="noopener noreferrer" class="text-pink-600 hover:underline">GitHub</a>
